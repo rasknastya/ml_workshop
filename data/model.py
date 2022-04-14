7dd48fc4d3ea62aa0874b6c078fb3b2df2fecc9e
rasknastya <rasknastya@gmail.com>
2022-04-14 13:05:01 +0600

    Setup removed

--- data/model.py
+++ data/model.py
@@ -18,21 +18,6 @@ TA = Union[T, ARRAY]
 D = torch.device
 CPU = torch.device('cpu')
 
-
-def get_device(device_id: int) -> D:
-    if not torch.cuda.is_available():
-        return CPU
-    device_id = min(torch.cuda.device_count() - 1, device_id)
-    return torch.device(f'cuda:{device_id}')
-
-
-CUDA = get_device
-
-current_directory = os.getcwd()
-save_path = os.path.join(os.path.dirname(current_directory), "pretrained_models")
-os.makedirs(save_path, exist_ok=True)
-model_path = os.path.join(save_path, 'model_wieghts.pt')
-
 class MLP(nn.Module):
 
     def forward(self, x: T) -> T:
